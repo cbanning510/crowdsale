119cc3dc18807c5018b3144452ba5496d0fe3b3f
Chris Banning <chrisbanning510@gmail.com>
2026-04-10 01:02:22 -0400

    buyTokens for free implemented

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,6 +1,18 @@
 // SPDX-Licens-Identifier: Unilicense
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Crowdsale {
-    string public name = "Crowdsale";
+    Token public token;
+
+    // Need code
+    // Need address
+    constructor(Token _token) {
+        token = _token;
+    }
+
+    function buyTokens(uint256 _amount) public {
+        token.transfer(msg.sender, _amount);
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -1,20 +1,62 @@
-const { ethers } = require("hardhat");
-const { expect } = require("chai");
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), "ether");
+  return ethers.utils.parseUnits(n.toString(), 'ether');
 };
 
 describe('Crowdsale', () => {
-    let crowdsale;
-    beforeEach(async() => {
-        const Crowdsale = await ethers.getContractFactory("Crowdsale");
-            crowdsale = await Crowdsale.deploy();
-    })
+  let crowdsale, token;
+  let accounts, deployer, user1;
 
-    describe("Deployment", () => {
-        it("has correct name", async () => {            
-            expect(await crowdsale.name()).to.eq("Crowdsale");
-        })
-    })
-})
+  beforeEach(async () => {
+    // Load Contracts
+    const Crowdsale = await ethers.getContractFactory('Crowdsale');
+    const Token = await ethers.getContractFactory('Token');
+
+    // Deploy token
+    token = await Token.deploy('Chris University', 'CHRISU', '1000000');
+
+    // Configure Accounts
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    user1 = accounts[1];
+
+    // Deploy Crowdsale
+    crowdsale = await Crowdsale.deploy(token.address);
+
+    //Send tokens to crowdsale
+    let transaction = await token
+      .connect(deployer)
+      .transfer(crowdsale.address, tokens(1000000));
+    await transaction.wait();
+  });
+
+  describe('Deployment', () => {
+    it('sends tokens to the Crowdsale contract', async () => {
+      expect(await token.balanceOf(crowdsale.address)).to.equal(
+        tokens(1000000)
+      );
+    });
+
+    it('returns token address', async () => {
+      expect(await crowdsale.token()).to.equal(token.address);
+    });
+  });
+
+  describe('Buying Tokens', () => {
+    let amount = tokens(10);
+
+    describe('Success', () => {
+      it('transfers tokens', async () => {
+        let transaction = await crowdsale.connect(user1).buyTokens(amount);
+        let result = await transaction.wait();
+
+        expect(await token.balanceOf(crowdsale.address)).to.equal(
+          tokens(999990)
+        );
+        expect(await token.balanceOf(user1.address)).to.equal(amount);
+      });
+    });
+  });
+});
